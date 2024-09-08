a867ee9199c31802f088f06a72570178ba56f211
Muhammad Ahmad <ahmadshaukat_4@outlook.com>
2024-09-08 14:11:39 +0500

    feat: integrate backend chat api

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -4,26 +4,43 @@ export default async function handler(req, res) {
 	}
 
 	const { messages } = req.body;
+	const messagesLength = messages.length;
+
+	console.log("messages", messages);
+	console.log("messages", messages[messagesLength - 1].content);
 
 	try {
-		const response = await fetch("https://api.openai.com/v1/chat/completions", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-				Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
-			},
-			body: JSON.stringify({
-				model: "gpt-3.5-turbo",
-				messages: messages,
-			}),
-		});
+		const response = await fetch(
+			`https://c15e-34-106-76-61.ngrok-free.app/langchain?input=${
+				messages[messagesLength - 1].content
+			}`,
+			{
+				method: "GET",
+				headers: {
+					"Content-Type": "application/json",
+					// Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+				},
+				// body: JSON.stringify({
+				// 	model: "gpt-3.5-turbo",
+				// 	messages: messages,
+				// }),
+			}
+		);
 
 		if (!response.ok) {
 			throw new Error("Failed to get response from OpenAI");
 		}
 
 		const data = await response.json();
-		res.status(200).json({ content: data.choices[0].message.content });
+
+		// {
+		//   "question": "hi",
+		//   "chat_history": [],
+		//   "output": "Hello! How can I assist you today?"
+		// }
+
+		// res.status(200).json({ content: data.choices[0].message.content });
+		res.status(200).json({ content: data.output });
 	} catch (error) {
 		console.error("Error:", error);
 		res
